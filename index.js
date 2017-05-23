095caa10c81274606410d7f0b4496c07d5e3ff49
jonabasque <jonmultimedia@gmail.com>
2017-05-23 18:35:33 +0200

    Removed walkRules() & used colors module

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 var postcss = require('postcss');
 
-//var colors = require('./colors');
+//this package modules
+var colors = require('./colors');
 
 module.exports = postcss.plugin('more-colors', function (opts) {
     opts = opts || {};
     // Work with options here
 
     return function (root, result) {
-        console.log(root);
-
-      	// Transform CSS AST here
-        root.walkRules(function(rule){
-            // Transform each rule here
-            rule.walkDecls(function(decl){
-                // Transform each property declaration here
-                console.log(decl);
-              	decl.value = decl.value.replace('lightblack', '#444');
-            	decl.value = decl.value.replace('lightpink', '#d47');
-              	decl.value = decl.value.replace('lightred', '#e32');
+        //console.log(root);
+       	// Transform CSS AST here
+        root.walkDecls(function(decl){
+            // Transform each property declaration here
+            //console.log(decl);
+            Object.keys(colors).forEach(function(color) {
+                if (decl.value.search(color) != -1) {
+                    var colorValue = colors[color];
+                    //console.log(colorValue);
+                    decl.value = decl.value.replace(color, colorValue);
+                }
             });
         });
     };
